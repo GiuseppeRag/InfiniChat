a1d5dc9a30b9627d7bff4df17fcfb789ecfcaef8
Joey <joeyr699@gmail.com>
2020-03-04 13:56:41 -0500

    quality of life changes

--- public/chatFunctions.js
+++ public/chatFunctions.js
@@ -1,5 +1,6 @@
 $(() => {
-    var socket = io.connect('https://infinichat-application.herokuapp.com/')
+    //var socket = io.connect('https://infinichat-application.herokuapp.com/')
+    var socket = io.connect('http://localhost:3000/')
 
     var changeUsername = $("#btnChangeUsername")
     var username = $('#txtUsername')
@@ -10,6 +11,8 @@ $(() => {
     var roomTwo = $('#roomTwo')
     var roomThree = $('#roomThree')
     var roomFour = $('#roomFour')
+    var roomMessage = $('#roomMessage')
+    var newUserMessage = $('#newUserMessage')
 
     changeUsername.click(() => {
         socket.emit('changeUsername', {"username": username.val()})
@@ -24,19 +27,54 @@ $(() => {
         chatroom.append(`<p>${messageData.username}: ${messageData.message}</p>`)
     })
 
+    socket.on('welcome', (user) => {
+        displayMessage('Welcome to InfiniChat! You are currently logged in as ' + user.username)
+        username.val(user.username)
+    })
+
+    socket.on('newUser', (user) => {
+        userLog(user.username + ' has logged into InfiniChat')
+    })
+
+    socket.on('userLogout', (user) => {
+        userLog(user.username + ' has logged out of InfiniChat')
+    })
+
+    socket.on('usernameChange', (user) => {
+        userLog(user.oldUsername + ' has changed their name to ' + user.newUsername)
+    })
+
     roomOne.click(() => {
         socket.emit('joinRoom', {"room": "Room One"})
+        displayMessage('You are currently in Room One')
     })
 
     roomTwo.click(() => {
         socket.emit('joinRoom', {"room": "Room Two"})
+        displayMessage('You are currently in Room Two')
     })
 
     roomThree.click(() => {
         socket.emit('joinRoom', {"room": "Room Three"})
+        displayMessage('You are currently in Room Three')
     })
 
     roomFour.click(() => {
         socket.emit('joinRoom', {"room": "Room Four"})
+        displayMessage('You are currently in Room Four')
     })
+
+    const displayMessage = (message) => {
+        roomMessage.stop()
+        roomMessage.fadeIn(250)
+        roomMessage.text(message).delay(3000)
+        roomMessage.fadeOut(500)
+    }
+
+    const userLog = (message) => {
+        newUserMessage.stop()
+        newUserMessage.fadeIn(250)
+        newUserMessage.text(message).delay(3000)
+        newUserMessage.fadeOut(500)
+    }
 })
